4c971b510031a0b37187d09f100de9c2783811f1
devanet <pj@devanet.de>
2015-02-02 20:38:36 +0100

    Dont crash, display error to user instead

---
+++ RBTVSendeplanCS/OnErrorEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBTVSendeplanCS
+{
+    public class OnErrorEventArgs : EventArgs
+    {
+        private Exception m_exception;
+        public Exception Error
+        {
+            set {}
+            get { return m_exception; }
+        }
+
+        public OnErrorEventArgs(Exception ex)
+        {
+            m_exception = ex;
+        }
+    }
+}
